6f3838e3426d3b7e76f5f199ae4e0a95f4da06d4
benjasx <benjasanchez175@gmail.com>
2023-12-13 19:22:54 -0700

    ejercicio-checkbox

---
+++ form-check/pagina1.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP-checkbox</title>
+</head>
+<body>
+    <form action="pagina2.php" method="post">
+        <h2>operandao</h2>
+        ingresa el valor 1 <br>
+        <input type="text" name="valor1">
+        <br>
+        ingresa el valor 2 <br>
+        <input type="text" name="valor2">
+        <br>
+        <input type="checkbox" name="check1" value="sumar">sumar
+        <br>
+        <input type="checkbox" name="check2" value="restar">restar 
+        <br>
+        <input type="submit" value="Enviar">
+    </form>
+</body> 
+</html>

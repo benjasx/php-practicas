e32477e70792594e7b6bf362fc7ae2e4db859ec4
benjasx <benjasanchez175@gmail.com>
2023-12-13 18:37:18 -0700

    ejercicio-seleccionar grado de estudios

---
+++ form-3radiobtn/pagina1.php
@@ -0,0 +1,26 @@
+<html>
+
+	<head>
+  		<title>Problema</title>
+	</head>
+
+	<body>
+
+  		<form action="pagina2.php" method="post">
+    			Ingrese su nombre:
+    			<input type="text" name="name">
+    			<br>
+    			 Seleccione su grado de estudios:
+    			<br>
+    			<input type="radio" name="estudios" value="1-primaria">1-primaria
+    			<br>
+    			<input type="radio" name="estudios" value="2-secundaria">2-secundaria
+    			<br>
+			    <input type="radio" name="estudios" value="3-ninguno">3-ninguno
+                <br>
+    			<input type="submit" name="Enviar">
+  		</form>
+
+	</body>
+
+</html>
---
+++ form-3radiobtn/pagina2.php
@@ -0,0 +1,13 @@
+<html>
+
+	<head>
+  		<title>Problema</title>
+	</head>
+
+	<body>
+        <?php
+  		echo $_REQUEST['name']. " estudio hasta " . $_REQUEST['estudios'];
+        ?>
+	</body>
+
+</html>
